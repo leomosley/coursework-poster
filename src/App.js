8f2191b5384c7511c2c45f184f37cb3c8c7ea826
leomosley <leo.mosley2@gmail.com>
2023-11-16 23:40:16 +0000

    Text box popup updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { createRef, useEffect } from "react";
 import "./App.css";
 
 // Components
--- src/components/Sections.js
+++ src/components/Sections.js
@@ -57,15 +57,17 @@ function Sections() {
     }, []);
 
     return (
+      <>
+      <div id="ghp-text-box" className={`text-box ghp ${active? "active" : ""}`}>
+        <p className="section-text main tb">The greatest happiness principle (GHP) holds that we should pursue actions that produce the greatest amount of overall happiness in the world, not just maximize our own happiness ("J. S. Mill's Utilitarianism," n.d.). <a className="section-text main url" target="_blank" href="https://philife.nd.edu/j-s-mills-utilitarianism-promote-the-most-happiness/#:~:text=The%20greatest%20happiness%20principle%20(GHP,just%20maximize%20our%20own%20happiness.">More...</a></p>
+      </div>
       <div className="section-1">
-        <div id="ghp-text-box" className={`text-box ghp ${active? "active" : ""}`}>
-          <p className="section-text main tb">The greatest happiness principle (GHP) holds that we should pursue actions that produce the greatest amount of overall happiness in the world, not just maximize our own happiness ("J. S. Mill's Utilitarianism," n.d.). <a className="section-text main url" target="_blank" href="https://philife.nd.edu/j-s-mills-utilitarianism-promote-the-most-happiness/#:~:text=The%20greatest%20happiness%20principle%20(GHP,just%20maximize%20our%20own%20happiness.">More...</a></p>
-        </div>
         <p className="section-text title">Selecting an Ethical Framework</p>
         <p className="section-text main"><b>Utilitarianism</b> is my chosen ethical framework for measuring risk - its fundamental principle is providing the most utility to the largest number of people. </p>
         <p className="section-text main">In this scenario largest number of people are pedestrians and the passengers of other vehicles. Following the <strong id="ghp-text">Greatest Happiness Principle</strong>, they are who should be provided the most utility.</p>
         <p className="section-text main bottom">Within this context utility can be defined as minimising negative impacts - in order to do so areas of high risk should be avoided.</p>
       </div>
+      </>
     );
   }
 
