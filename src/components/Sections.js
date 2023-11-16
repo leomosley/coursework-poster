1dbaf0c77b8cef8621dea1194a941c6d38c723dc
leomosley <leo.mosley2@gmail.com>
2023-11-16 22:40:08 +0000

    Updated clip paths

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -59,43 +59,59 @@ function Sections() {
     return (
       <div className="section-1">
         <div id="ghp-text-box" className={`text-box ghp ${active? "active" : ""}`}>
-          <p className="section-text main tb">The greatest happiness principle (GHP) holds that we should pursue actions that produce the greatest amount of overall happiness in the world, not just maximize our own happiness. <a className="section-text main url" target="_blank" href="https://philife.nd.edu/j-s-mills-utilitarianism-promote-the-most-happiness/#:~:text=The%20greatest%20happiness%20principle%20(GHP,just%20maximize%20our%20own%20happiness.">More...</a></p>
+          <p className="section-text main tb">The greatest happiness principle (GHP) holds that we should pursue actions that produce the greatest amount of overall happiness in the world, not just maximize our own happiness ("J. S. Mill's Utilitarianism," n.d.). <a className="section-text main url" target="_blank" href="https://philife.nd.edu/j-s-mills-utilitarianism-promote-the-most-happiness/#:~:text=The%20greatest%20happiness%20principle%20(GHP,just%20maximize%20our%20own%20happiness.">More...</a></p>
         </div>
         <p className="section-text title">Selecting an Ethical Framework</p>
         <p className="section-text main"><b>Utilitarianism</b> is my chosen ethical framework for measuring risk - its fundamental principle is providing the most utility to the largest number of people. </p>
         <p className="section-text main">In this scenario largest number of people are pedestrians and the passengers of other vehicles. Following the <strong id="ghp-text">Greatest Happiness Principle</strong>, they are who should be provided the most utility.</p>
         <p className="section-text main bottom">Within this context utility can be defined as minimising negative impacts - in order to do so areas of high risk should be avoided.</p>
-    </div>
+      </div>
     );
   }
 
+  const SectionTwo = () => {
+
+    return (
+      <div className="section-2">
+        <div className="section-2-content top">
+          <p className="section-text title">Assiging Edge Weights</p>
+          <p className="section-text main"></p>
+        </div>
+        <div className="section-2-content middle">
+          <p className="section-text main"></p>
+        </div>
+        <div className="section-2-content bottom">
+          <p className="section-text main"></p>
+        </div>
+      </div>
+    );
+  }
+
+  const SectionThree = () => {
+
+    return (
+      <div className="section-3">
+        <p className="section-text title left-margin">The Algorithm</p>
+        <p className="section-text main left-margin">text</p>
+      </div>
+    );
+  }
+
+  const SectionFour = () => {
+
+    return (
+      <div className="section-4">
+        <p className="section-text title left-margin">Paths</p>
+        <p className="section-text main left-margin">texts</p>
+      </div>
+    );
+  }
   return (
     <>
     <SectionOne />
-    <div className="section-1-rs"></div>
-    <div className="section-2">
-      <div className="section-2-content top">
-        <p className="section-text title">Assiging Edge Weights</p>
-        <p className="section-text main"></p>
-      </div>
-      <div className="section-2-content middle">
-        <p className="section-text main"></p>
-      </div>
-      <div className="section-2-content bottom">
-        <p className="section-text main"></p>
-      </div>
-    </div>
-    <div className="section-2-rs"></div>
-    <div className="section-3">
-      <p className="section-text title">The Algorithm</p>
-      <p className="section-text main"></p>
-    </div>
-    <div className="section-3-ls"></div>
-    <div className="section-4">
-      <p className="section-text title">Paths</p>
-      <p className="section-text main"></p>
-    </div>
-    <div className="section-4-ls"></div>
+    <SectionTwo />
+    <SectionThree />
+    <SectionFour />
     </>
   )
 }
