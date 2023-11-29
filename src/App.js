8259af249f47713ed6c79ca1caccf7e376a341e2
leomosley <leo.mosley2@gmail.com>
2023-11-29 10:27:30 +0000

    Fixed error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import { icons } from "./constants";
 
 function App() {
   const [orientation, setOrientation] = useState(true);
-  console.log(test);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
